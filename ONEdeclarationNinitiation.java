5c460f8f1ff465aab5ccbcdb3f05ec26f3281766
Zishan Pathan <141132499+Zishan-Khan01@users.noreply.github.com>
2026-06-18 13:17:56 +0530

    Add files via upload

---
+++ ONEdeclarationNinitiation.java
@@ -0,0 +1,9 @@
+//Strings in java are IMMUTABLE
+public class ONEdeclarationNinitiation{
+	public static void main(String args[]){
+		//methods of declaring & initiating a string
+		String str = "abcd";
+		String str1 = new String("xyz");
+	}
+
+}
